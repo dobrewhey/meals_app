277b67be62cef7f2fd77ffb39faad72ffbe97b1d
dobrewhey <fiejtekmichal@gmail.com>
2024-01-24 15:20:35 +0100

    Added drawer

--- meals_app/lib/screens/meal_details.dart
+++ meals_app/lib/screens/meal_details.dart
@@ -2,18 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 
 class MealDetailsScreen extends StatelessWidget {
-  const MealDetailsScreen({super.key, required this.meal});
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+    required this.onToggleFavorite,
+  });
 
   final Meal meal;
+  final void Function(Meal meal) onToggleFavorite;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-        meal.title,
-        style: const TextStyle(color: Colors.white),
-      )),
+        title: Text(
+          meal.title,
+          style: const TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              onToggleFavorite(meal);
+            },
+            icon: const Icon(Icons.star),
+          )
+        ],
+      ),
       body: SingleChildScrollView(
         child: Column(children: [
           Image.network(
